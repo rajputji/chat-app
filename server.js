3da00ea9391f2b06d91da41c3005efe8fa68fca3
Abhishek Rajput <abhishekrajput676@gmail.com>
2020-12-15 08:21:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ io.on('connection', () => {
   console.log('new user connected')
 })
 
-var server = http.listen(3000, () => {
+var server = http.listen( process.env.PORT  || 3000, () => {
   console.log('server is running on port', server.address().port);
 });
